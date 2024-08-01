8325d6c6594bcb8a10c8bdea93773e70bab33814
susheepls <169642127+susheepls@users.noreply.github.com>
2024-08-01 12:15:12 +0900

    Getting started with logged in options

---
+++ src/interfaces/card.interface.ts
@@ -0,0 +1,10 @@
+export interface Card {
+    back_text: string;
+    category_id: number;
+    created_at: string;
+    front_text: string;
+    id: number;
+    updated_at: string;
+    user_id: number;
+    views: number | null;
+}
