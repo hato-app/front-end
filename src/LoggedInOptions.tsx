4ee6bec340f80f4995941f8a16c0db59c8a6c0b0
susheepls <169642127+susheepls@users.noreply.github.com>
2024-08-01 16:36:51 +0900

    Feat: get skeleton working for LoggedInOption

--- src/LoggedInOptions.tsx
+++ src/LoggedInOptions.tsx
@@ -6,33 +6,63 @@ interface LoggedInProps {
 }
 const LoggedInOptions:React.FC<LoggedInProps> = ({isLoggedIn}) => {
     //useStates
-    const [cardFront, setCardFront] = useState<string>();
-    const [cardBack, setCardBack] = useState<string>();
-
+    const [cardFront, setCardFront] = useState<string>('');
+    const [cardBack, setCardBack] = useState<string>('');
+    const [createdCard, setCreatedCard] = useState<Card | null>(null);
+    const [isMakingCard, setIsMakingCard] = useState<boolean>(false)
     //useEffects
 
-    //handleFunctions
-    async function createCard() {
-
-        let req:Card[] = await (await fetch(import.meta.env.VITE_SERVER+"/cards/1")).json()
-        console.log(req)
+    //handleFunctions 
+    async function createCard() {  
+        const res = await fetch(import.meta.env.VITE_SERVER+"/cards/", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                },
+            body: JSON.stringify({
+                front_text: cardFront,
+                back_text: cardBack,
+            }),
+        });
+        const created = (await res.json());
+        setCreatedCard(created);
+    }
+    function handleMakingCard() {
+        !isMakingCard ? setIsMakingCard(true) : setIsMakingCard(false);
     }
 
-    return <>
-    {isLoggedIn ? (
-        <div className="LoggedInOptions">
-            {/* <Comments/> */}
-            <div className='create_'>
-                <button onClick={createCard}>Create your own card!</button>
-                <button>Get another random message!</button>
-            </div>
+
+    return <div className='belowCard'>
+        {/* <Comments/> */}
+        <div>
+            {!isMakingCard ? (
+                <div className='loggedInButtons'>
+                    <button onClick={handleMakingCard} disabled={!isLoggedIn} >Create a card!</button>
+                    <button>Get another random message!</button>
+                 </div>
+                ) : (
+                <div className='cardCreation'>
+                    <div className='inputCreate'>
+                    <textarea rows={5}cols={33}  placeholder="FRONT"  />
+                    </div>
+                    <div className='creationButtons'>
+                        <button onClick={handleMakingCard}>submit</button>
+                        <button onClick={handleMakingCard}>cancel</button>
+                    </div>
+                </div>
+            )}
         </div>
-    ) : (
+    
+      
+
+
+
         <div className='notLoggedIn'>
             <h1>Log in to create, like, comment !</h1>
         </div>
-    )}
-    </>
+
+    </div>
 }
 
 export default LoggedInOptions;
