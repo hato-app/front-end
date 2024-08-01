9e33d53cc00538ff86b5b7e72b5ec9aa0f227391
Brandon Cook <bcook2289@gmail.com>
2024-08-01 09:58:34 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-    </>
-  )
+  return <>hello</>;
 }
 
-export default App
+export default App;
