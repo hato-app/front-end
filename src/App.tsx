ca7af8ad90c2c22ecc7d085919c705cee5d70ea5
Brandon Cook <bcook2289@gmail.com>
2024-08-01 11:58:41 +0900

    feat: completed first pass at handling cards on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Cards from "./Cards";
+
+const server = import.meta.env.VITE_SERVER;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isFrontOrBack, setIsFrontOrBack] = useState(false);
+  const [data, setData] = useState({
+    id: 1,
+    user_id: 1,
+    category_id: "jokes",
+    front_text: "What do you call a pony with a cough?",
+    back_text: "A little horse",
+    views: 0,
+  });
 
-  return <>hello</>;
+  useEffect(() => {
+    handleGetCard();
+  }, []);
+
+  async function handleGetCard(category = 3) {
+    // this gets every card
+    if (category === 3) {
+      const getAll = `${server}/cards`;
+      const response = await fetch(getAll);
+      const data = await response.json();
+      const dataLength: number = data.length;
+      const randNum: number = dataLength * Math.random();
+
+      const getID = `${getAll}/${randNum}`;
+      const responseID = await fetch(getID);
+      const dataID = await responseID.json();
+      setData(dataID);
+    } else {
+      const getAllCategory = `${server}/category/${category}`;
+      const responseCategory = await fetch(getAllCategory);
+      const dataCategory = await responseCategory.json();
+      const cards = dataCategory.map((card: []) => {
+        return card;
+      });
+      setData(cards[cards.length * Math.random()]);
+    }
+  }
+  function handleIsFrontOrBack() {
+    setIsFrontOrBack(!isFrontOrBack);
+  }
+  return (
+    <>
+      <Cards data={data} setCardOnClick={handleIsFrontOrBack} />
+    </>
+  );
 }
 
 export default App;
